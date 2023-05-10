1fd11933ba1cb0b2efc765c0f3cd212dc2fa6da5
Kimsumin123 <tntn211@naver.com>
2023-05-10 14:07:46 +0900

    ll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Chart from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = () => {
+
+  let data =  {
+      labels: ['4월 2일', '4월 5일', '4월 14일', '5월 3일', '5월 8일'],
+      datasets: [
+        {
+          label: '공포',
+          data: [2, 12, 12, 12, 2],
+          borderWidth: 1
+        },
+        {
+          label: '분노',
+          data: [12, 19, 3, 5, 9],
+          borderWidth: 1
+        },{
+          label: '슬픔',
+          data: [8, 14, 31, 23, 12],
+          borderWidth: 1
+        },{
+          label: '혐오',
+          data: [6, 23, 15, 28, 32],
+          borderWidth: 1
+        },{
+          label: '행복',
+          data: [2, 12, 12, 12, 2],
+          borderWidth: 1
+        },
+        {
+          label: '놀람',
+          data: [12, 19, 3, 5, 9],
+          borderWidth: 1
+        },{
+          label: '중립',
+          data: [8, 14, 31, 23, 12],
+          borderWidth: 1
+        },
+      ],
+    };
+    
+	return (
+    	<div>
+        	<Bar type="Bar" data={data} />
+        </div>
+    );
+    
 }
 
-export default App;
+export default app;
